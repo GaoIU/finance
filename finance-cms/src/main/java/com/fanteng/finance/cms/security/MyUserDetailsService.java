00ca6c39577dd9162eba3e5696134a2da981b486
猪儿虫 <@192.168.56.1>
2018-07-01 19:21:55 +0800

    添加security配置

---
+++ finance-cms/src/main/java/com/fanteng/finance/cms/security/MyUserDetailsService.java
@@ -0,0 +1,14 @@
+package com.fanteng.finance.cms.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class MyUserDetailsService implements UserDetailsService {
+
+	@Override
+	public UserDetails loadUserByUsername(String arg0) throws UsernameNotFoundException {
+		return null;
+	}
+
+}
