b81402ad91200a24ae027fb2e159b3d8d377547a
猪儿虫 <@192.168.56.1>
2018-07-04 00:16:04 +0800

    添加后台用户登录接口，完善security验证

--- finance-cms/src/main/java/com/fanteng/finance/cms/security/MyUserDetailsService.java
+++ finance-cms/src/main/java/com/fanteng/finance/cms/security/MyUserDetailsService.java
@@ -1,14 +1,99 @@
 package com.fanteng.finance.cms.security;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.util.AntPathMatcher;
+
+import com.fanteng.core.Operation;
+import com.fanteng.exception.ResourceErrorException;
+import com.fanteng.finance.cms.service.SysResourceService;
+import com.fanteng.finance.cms.service.SysRoleService;
+import com.fanteng.finance.cms.service.SysUserService;
+import com.fanteng.finance.entity.SysResource;
+import com.fanteng.finance.entity.SysRole;
+import com.fanteng.finance.entity.SysUser;
+import com.fanteng.util.StringUtil;
 
 public class MyUserDetailsService implements UserDetailsService {
 
+	@Autowired
+	private SysUserService sysUserService;
+
+	@Autowired
+	private SysRoleService sysRoleService;
+
+	@Autowired
+	private SysResourceService sysResourceService;
+
+	private AntPathMatcher antPathMatcher = new AntPathMatcher();
+
+	@Value("${sys.role.admin.code}")
+	private String ADMINISTRATOR;
+
 	@Override
-	public UserDetails loadUserByUsername(String arg0) throws UsernameNotFoundException {
-		return null;
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+		SysUser sysUser = sysUserService.findOne("userName", Operation.EQ, userName);
+		return new User(userName, sysUser.getPassword(), getRoles(sysUser.getId()));
+	}
+
+	private Set<GrantedAuthority> getRoles(String sysUserId) {
+		Set<GrantedAuthority> authorities = new HashSet<GrantedAuthority>(0);
+
+		List<SysResource> list = sysResourceService.getResourcesBySysUserId(sysUserId);
+		for (SysResource sysResource : list) {
+			GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(
+					sysResource.getCode() + "@" + sysResource.getMethod());
+			authorities.add(grantedAuthority);
+		}
+
+		return authorities;
+	}
+
+	public boolean hasPermission(HttpServletRequest request, Authentication authentication) {
+		List<SysUser> resources = sysUserService.findOnes("url", Operation.EQ, request.getRequestURI());
+		if (CollectionUtils.isEmpty(resources)) {
+			throw new ResourceErrorException("无效的访问");
+		}
+
+		boolean hasPermission = false;
+
+		Object principal = authentication.getPrincipal();
+		if (principal instanceof UserDetails) {
+			String userName = ((UserDetails) principal).getUsername();
+			SysUser sysUser = sysUserService.findOne("userName", Operation.EQ, userName);
+
+			List<SysResource> list = sysResourceService.getResourcesBySysUserId(sysUser.getId());
+			for (SysResource sysResource : list) {
+				if (antPathMatcher.match(request.getRequestURI(), sysResource.getUrl())) {
+					hasPermission = true;
+					break;
+				}
+			}
+
+			List<SysRole> sysRoles = sysRoleService.getSysRolesBySysUserId(sysUser.getId());
+			for (SysRole sysRole : sysRoles) {
+				if (StringUtil.equals(ADMINISTRATOR, sysRole.getCode())) {
+					hasPermission = true;
+					break;
+				}
+			}
+		}
+
+		return hasPermission;
 	}
 
 }
---
+++ finance-cms/src/main/java/com/fanteng/finance/cms/sys/controller/AuthenticationController.java
@@ -0,0 +1,62 @@
+package com.fanteng.finance.cms.sys.controller;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.collections.MapUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fanteng.core.JsonResult;
+import com.fanteng.finance.cms.service.SysUserService;
+import com.fanteng.finance.entity.SysUser;
+
+@RestController
+public class AuthenticationController {
+
+	@Autowired
+	private SysUserService sysUserService;
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Value("${sys.user.default.session.key}")
+	private String default_session_key;
+
+	/**
+	 * 后台用户登录
+	 * 
+	 * @param session
+	 * @param param
+	 * @return
+	 */
+	@PostMapping("/signIn")
+	public JsonResult signIn(HttpSession session, @RequestBody Map<String, Object> param) {
+		JsonResult jsonResult = sysUserService.signIn(param);
+		if (jsonResult.getCode() != com.fanteng.core.HttpStatus.OK) {
+			return jsonResult;
+		}
+
+		String userName = MapUtils.getString(param, "userName");
+		String password = MapUtils.getString(param, "password");
+		SysUser sysUser = (SysUser) jsonResult.getData();
+
+		UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userName,
+				password);
+		Authentication authentication = authenticationManager.authenticate(authenticationToken);
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		session.setAttribute("SPRING_SECURITY_CONTEXT", SecurityContextHolder.getContext());
+		session.setAttribute(default_session_key, sysUser);
+
+		return jsonResult;
+	}
+
+}
