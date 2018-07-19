6f288df5199d0e1320aec9ec8062bb951ae6ac18
猪儿虫 <@192.168.1.36>
2018-07-19 18:04:44 +0800

    修改根据后台用户ID获取资源接口

--- finance-api/src/main/java/com/fanteng/finance/cms/service/impl/SysResourceServiceImpl.java
+++ finance-api/src/main/java/com/fanteng/finance/cms/service/impl/SysResourceServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.fanteng.core.Condition;
@@ -13,10 +14,13 @@ import com.fanteng.core.base.BaseServiceImpl;
 import com.fanteng.finance.cms.dao.SysResourceDao;
 import com.fanteng.finance.cms.service.SysResourceService;
 import com.fanteng.finance.cms.service.SysRoleResourceService;
+import com.fanteng.finance.cms.service.SysRoleService;
 import com.fanteng.finance.cms.service.SysUserRoleService;
 import com.fanteng.finance.entity.SysResource;
+import com.fanteng.finance.entity.SysRole;
 import com.fanteng.finance.entity.SysRoleResource;
 import com.fanteng.finance.entity.SysUserRole;
+import com.fanteng.util.StringUtil;
 
 @Service
 public class SysResourceServiceImpl extends BaseServiceImpl<SysResourceDao, SysResource> implements SysResourceService {
@@ -27,6 +31,12 @@ public class SysResourceServiceImpl extends BaseServiceImpl<SysResourceDao, SysR
 	@Autowired
 	private SysRoleResourceService sysRoleResourceService;
 
+	@Autowired
+	private SysRoleService sysRoleService;
+
+	@Value("${sys.role.admin.code}")
+	private String ADMINISTRATOR;
+
 	/**
 	 * 根据用户ID获取所有资源
 	 * 
@@ -35,6 +45,26 @@ public class SysResourceServiceImpl extends BaseServiceImpl<SysResourceDao, SysR
 	 */
 	@Override
 	public List<SysResource> getResourcesBySysUserId(String sysUserId) {
+		boolean hasPermission = false;
+
+		List<SysRole> sysRoles = sysRoleService.getSysRolesBySysUserId(sysUserId);
+		for (SysRole sysRole : sysRoles) {
+			if (StringUtil.equals(ADMINISTRATOR, sysRole.getCode())) {
+				hasPermission = true;
+				break;
+			}
+		}
+
+		List<Condition> conditions = new ArrayList<Condition>(0);
+		Condition status = new Condition("status", Operation.EQ, SysResource.status_normal);
+		Condition orderBy = new Condition("sort", Operation.ASC, "sort");
+		conditions.add(status);
+		conditions.add(orderBy);
+
+		if (hasPermission) {
+			return findAll(conditions);
+		}
+
 		List<SysUserRole> sysUserRoles = sysUserRoleService.findOnes("sysUserId", Operation.EQ, sysUserId);
 		if (CollectionUtils.isNotEmpty(sysUserRoles)) {
 			List<String> sysRoleIds = new ArrayList<String>(0);
@@ -50,14 +80,9 @@ public class SysResourceServiceImpl extends BaseServiceImpl<SysResourceDao, SysR
 					sysResourceIds.add(sysRoleResource.getSysResourceId());
 				}
 
-				List<Condition> conditions = new ArrayList<Condition>(0);
 				Condition ids = new Condition("id", Operation.IN, sysResourceIds);
-				Condition status = new Condition("status", Operation.NE, SysResource.status_disable);
-				Condition orderBy = new Condition("sort", Operation.ASC, "sort");
 				conditions.add(ids);
-				conditions.add(status);
-				conditions.add(orderBy);
-				
+
 				return findAll(conditions);
 			}
 		}
