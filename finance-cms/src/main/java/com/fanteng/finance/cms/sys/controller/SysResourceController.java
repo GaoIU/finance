b1eda13607faab58420ccfd6fe388770f8f7e064
猪儿虫 <@DESKTOP-J79EDNR>
2018-09-28 17:11:20 +0800

    添加修改后台资源方法

--- finance-cms/src/main/java/com/fanteng/finance/cms/sys/controller/SysResourceController.java
+++ finance-cms/src/main/java/com/fanteng/finance/cms/sys/controller/SysResourceController.java
@@ -10,6 +10,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -52,6 +53,32 @@ public class SysResourceController {
 
 		Serializable id = sysResourceService.save(sysResource);
 		if (id != null) {
+			return new JsonResult(com.fanteng.core.HttpStatus.OK, "操作成功");
+		}
+
+		return new JsonResult(com.fanteng.core.HttpStatus.ACCEPTED, "操作失败");
+	}
+
+	/**
+	 * 修改后台资源
+	 * 
+	 * @param sysResource
+	 * @return
+	 */
+	@PutMapping
+	public JsonResult edit(@Valid @RequestBody SysResource sysResource) {
+		String code = sysResource.getCode().toUpperCase();
+		boolean checkCode = sysResourceService.checkCode(code, sysResource.getId());
+		if (checkCode) {
+			return new JsonResult(com.fanteng.core.HttpStatus.BAD_REQUEST, "该资源编码已被使用");
+		}
+
+		String method = sysResource.getMethod().toUpperCase();
+		sysResource.setCode(code);
+		sysResource.setMethod(method);
+
+		boolean issuccess = sysResourceService.updateIgnore(sysResource);
+		if (issuccess) {
 			return new JsonResult(com.fanteng.core.HttpStatus.OK, "操作成功");
 		}
 
